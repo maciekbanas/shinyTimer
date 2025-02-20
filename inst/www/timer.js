67455b4d49fdc84f7e0292ccd29577d895c18e47
Maciej Banas <banasmaciek@gmail.com>
2025-02-20 13:41:14 +0000

    Add 'stopwatch' format with display of centiseconds.

--- inst/www/timer.js
+++ inst/www/timer.js
@@ -1,48 +1,55 @@
 let timerInterval;
+
+function formatTime(time, format) {
+  if (format === 'clock') {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+  } else if (format === 'stopwatch') {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    const centiseconds = Math.floor((time * 100) % 100);
+    return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}:${String(centiseconds).padStart(2, '0')}`;
+  } else {
+    return Math.floor(time);
+  }
+}
+
 Shiny.addCustomMessageHandler('countDown', function(message) {
   const { inputId } = message;
   clearInterval(timerInterval);
   const countdownElement = document.getElementById(inputId);
-  let timeLeft = parseInt(countdownElement.getAttribute('data-start-time'), 10);
+  let timeLeft = parseFloat(countdownElement.getAttribute('data-start-time'));
   const format = countdownElement.getAttribute('data-format');
 
   countdownElement.textContent = formatTime(timeLeft, format);
-  
+
   timerInterval = setInterval(function() {
-    timeLeft--;
+    timeLeft -= 0.01;
     countdownElement.textContent = formatTime(timeLeft, format);
 
     if (timeLeft <= 0) {
       clearInterval(timerInterval);
+      countdownElement.textContent = formatTime(0, format);
       Shiny.setInputValue('timer_done', true);
     }
-  }, 1000);
+  }, 10);
 });
 
 Shiny.addCustomMessageHandler('countUp', function(message) {
   const { inputId } = message;
   clearInterval(timerInterval);
   const countElement = document.getElementById(inputId);
-  let timeStart = parseInt(countElement.getAttribute('data-start-time'), 10);
+  let timeStart = parseFloat(countElement.getAttribute('data-start-time'));
   const format = countElement.getAttribute('data-format');
 
   countElement.textContent = formatTime(timeStart, format);
   
   timerInterval = setInterval(function() {
-    timeStart++;
+    timeStart += 0.01;
     countElement.textContent = formatTime(timeStart, format);
-  }, 1000);
+  }, 10); // Update every 10 milliseconds for precision
 });
-
-function formatTime(time, format) {
-  if (format === 'clock') {
-    const minutes = Math.floor(time / 60);
-    const seconds = time % 60;
-    return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
-  } else {
-    return time;
-  }
-};
 
 Shiny.addCustomMessageHandler('stopTimer', function(message) {
   clearInterval(timerInterval);
@@ -72,7 +79,7 @@ Shiny.addCustomMessageHandler('updateShinyTimer', function(message) {
     labelElement.textContent = label;
   }
   
-  const currentStartTime = parseInt(countdownElement.getAttribute('data-start-time'), 10);
+  const currentStartTime = parseFloat(countdownElement.getAttribute('data-start-time'));
   const currentFormat = countdownElement.getAttribute('data-format');
   let timeLeft = currentStartTime;
 
