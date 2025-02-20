f4290ed3f7d0fcabbf1aca50e403c75c98e87d2b
Maciej Banas <banasmaciek@gmail.com>
2025-02-20 12:45:43 +0000

    Add a simple code example.

---
+++ inst/examples/shinymobile_timer_simple.R
@@ -0,0 +1,20 @@
+devtools::load_all()
+
+ui <- shinyMobile::f7Page(
+  shinyMobile::f7Card(
+    shinyTimer(
+      inputId = "shiny_timer",
+      seconds = 10L, 
+      format = "simple", 
+      style = "font-weight: bold; font-size: 72px; text-align:center"
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  shiny::observe({
+    countDown(session, "shiny_timer")
+  })
+}
+
+shinyApp(ui, server)
